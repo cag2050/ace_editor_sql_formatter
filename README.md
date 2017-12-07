07967a6146ce67a7403502e19a4b7a188863feed
cag2050 <cag2050@163.com>
2017-12-07 09:49:00 +0800

    m

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # ace_demo
 
 * 官方地址：https://github.com/ajaxorg/ace
-* 参考：https://yq.aliyun.com/articles/65260
+* 参考：
+1. https://yq.aliyun.com/articles/65260
+1. http://blog.csdn.net/sd4015700/article/details/49910353
+
+* 以下部分是设置输入代码提示的，如果不需要可以不用引用ext-language_tools.js。
+```
+    ace.require("ace/ext/language_tools");
+    editor.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true
+    });
+```
+*
+```
+editor.getValue();//获取内容
+editor.setValue(value);//设置内容
+```
 
 > A Vue.js project
 
--- src/components/Ace.vue
+++ src/components/Ace.vue
@@ -1,6 +1,8 @@
 <template lang='pug'>
-div.ace
-    pre#editor select * from user;
+.ace
+    button(@click='formatSql') 格式化
+    div 请输入sql：
+    pre#editor select * from user where uid =1;
 </template>
 
 <script>
@@ -14,22 +16,47 @@ export default {
         let ace = window.ace
         ace.require('ace/ext/old_ie')
         ace.require('ace/ext/language_tools')
+        ace.require('ace/ext/language_tools')
         var editor = ace.edit('editor')
         editor.$blockScrolling = Infinity
         editor.setFontSize(16)
-        editor.session.setMode('ace/mode/c_cpp')
+        // editor.session.setMode('ace/mode/c_cpp')
         editor.setOptions({
             enableBasicAutocompletion: true,
             enableSnippets: true,
-            enableLiveAutocompletion: true
+            enableLiveAutocompletion: true,
+            wrap: 'free'
         })
         editor.setTheme('ace/theme/monokai')
+        /*
+        var SqlMode = ace.require('ace/mode/sql').Mode
+        editor.session.setMode(new SqlMode())
+        */
+    },
+    methods: {
+        formatSql () {
+            let ace = window.ace
+            var editor = ace.edit('editor')
+            editor.setOption('wrap', 'free')
+            editor.session.setUseWrapMode(true)
+            var SqlMode = ace.require('ace/mode/sql').Mode
+            console.log(new SqlMode())
+            editor.session.setMode(new SqlMode())
+            /*
+            var code = editor.getValue()
+            editor.setValue(code + '1111')
+            */
+        }
     }
+
 }
 </script>
 
 <style lang='stylus'>
 .ace
+    text-align left
+    button
+        font-size 16px
     #editor
         width 600px
         height 400px
